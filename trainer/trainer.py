96575a7b0ead791cb2f737772c584830b53584a9
Steve Pennington <steve@pennington.email>
2023-03-24 08:30:09 -0700

    Refactor trainer code in preparation for new model
    Extracts out code that is specific to the token based models so
    that we can start adding a character based model.

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -10,26 +10,9 @@ from models import RecurrentCrosswordModel
 from training import Trainer
 from trainingstats import TrainingReporter
 
-PADDING_TOKEN_INDEX = 0
-PAD_TO_SIZE = 11
-
-def run(args):
-    device = args.device
-    if device == 'auto':        
-        #TODO: Add support for cuda
-        # attempt to run on mps - will do work on the GPU for MacOS
-        device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-    
-    print(f'Running on:', device)
-
-    # load data, split datasets, build vocabs
-    #TODO: Parameterize this
-    dataset = CrosswordClueAnswersDataset("cleaned_data/dupes_10_or_less_tokens.csv")
-    train_size = int(0.8 * len(dataset))
-    dev_size = int(0.1 * len(dataset))
-    test_size = len(dataset) - train_size - dev_size
-    g = torch.Generator().manual_seed(42) # this manual_seed is important to ensure that we consistently split the dataset
-    train_dataset, test_dataset, dev_dataset = torch.utils.data.random_split(dataset, [train_size, test_size, dev_size], generator=g)
+def build_token_model(train_dataset, device, args):
+    PADDING_TOKEN_INDEX = 0
+    PAD_TO_SIZE = 11
 
     # build vocab onlt off of training data (for now...)
     tokenizer = get_tokenizer('basic_english')
@@ -43,8 +26,25 @@ def run(args):
     answers_vocab = build_vocab_from_iterator(answers_iter, specials=['<unk>'])
     answers_vocab.set_default_index(0)
 
-    print(f'{len(dataset)=}\n{len(train_dataset)=}\n{len(test_dataset)=}\n{len(dev_dataset)=}')
     print(f'{len(answers_vocab)=}\n{len(clues_vocab)=}')
+
+    model = SimpleCrosswordModel(
+        vocab_size=len(clues_vocab),
+        embed_dim=args.embedding_dimensions,
+        input_size=PAD_TO_SIZE,
+        hidden_size=args.hidden_layer_size,
+        output_size=len(answers_vocab),
+        device=device,
+        hidden_depth=args.hidden_depth)
+
+    # model = RecurrentCrosswordModel(
+    #     vocab_size=len(clues_vocab),
+    #     output_size=len(answers_vocab),
+    #     embed_dim=args.embedding_dimensions,
+    #     hidden_size=args.hidden_layer_size,
+    #     hidden_depth=args.hidden_depth,
+    #     device=device
+    # )
 
     def collate_batch(batch):
         answer_list, clue_list = [], []
@@ -67,30 +67,34 @@ def run(args):
         clue_list = torch.tensor(clue_list).to(device)
 
         return answer_list, clue_list
+    
+    return model, collate_batch
+
+def run(args):
+    device = args.device
+    if device == 'auto':        
+        #TODO: Add support for cuda
+        # attempt to run on mps - will do work on the GPU for MacOS
+        device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    
+    print(f'Running on:', device)
+
+    # load data, split datasets, build vocabs
+    #TODO: Parameterize this
+    dataset = CrosswordClueAnswersDataset("cleaned_data/dupes_10_or_less_tokens.csv")
+    train_size = int(0.8 * len(dataset))
+    dev_size = int(0.1 * len(dataset))
+    test_size = len(dataset) - train_size - dev_size
+    g = torch.Generator().manual_seed(42) # this manual_seed is important to ensure that we consistently split the dataset
+    train_dataset, test_dataset, dev_dataset = torch.utils.data.random_split(dataset, [train_size, test_size, dev_size], generator=g)
+    print(f'{len(dataset)=}\n{len(train_dataset)=}\n{len(test_dataset)=}\n{len(dev_dataset)=}')
+
+    model, collate_batch = build_token_model(train_dataset=train_dataset, device=device, args=args)
 
     # shuffle the training dataloader so we go through different batches each time
     train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate_batch)
     dev_dataloader = DataLoader(dev_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate_batch) 
-
     print(f'{len(train_dataloader)=}\n{len(dev_dataloader)=}')
-
-    model = SimpleCrosswordModel(
-        vocab_size=len(clues_vocab),
-        embed_dim=args.embedding_dimensions,
-        input_size=PAD_TO_SIZE,
-        hidden_size=args.hidden_layer_size,
-        output_size=len(answers_vocab),
-        device=device,
-        hidden_depth=args.hidden_depth)
-
-    # model = RecurrentCrosswordModel(
-    #     vocab_size=len(clues_vocab),
-    #     output_size=len(answers_vocab),
-    #     embed_dim=args.embedding_dimensions,
-    #     hidden_size=args.hidden_layer_size,
-    #     hidden_depth=args.hidden_depth,
-    #     device=device
-    # )
 
     trainable_model_parameters = filter(lambda p: p.requires_grad, model.parameters())
     params_count = sum(p.numel() for p in model.parameters())
